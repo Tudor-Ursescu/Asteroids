f38c75a0723a371fb97c7e6fbb21cfafcee47688
Tudor-Ursescu <ursescu.tudor@gmail.com>
2025-09-20 20:41:26 +0200

    added rotation to the player: a for left and d for right

--- main.py
+++ main.py
@@ -16,8 +16,10 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.update(dt)
         player.draw(screen)
         pygame.display.flip()
+
         dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":

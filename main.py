c7229797cc5c66ec0e355bb0ab24622ae8cfba41
Tudor-Ursescu <ursescu.tudor@gmail.com>
2025-09-20 21:53:30 +0200

    added asteroid fields, asteroids are circles, spwanning in three dif sizes

--- main.py
+++ main.py
@@ -1,25 +1,40 @@
+import pygame
 from constants import *
 from player import Player
-import pygame
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
-    clock = pygame.time.Clock()
-    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    AsteroidField.containers = (updatable,)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    Player.containers = (updatable, drawable)
+    
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroidfield = AsteroidField()
+
+    dt = 0
     while(1):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
+        updatable.update(dt)
+        
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+        
+        for item in drawable:
+            item.draw(screen)
+        
         pygame.display.flip()
-
+        
         dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
